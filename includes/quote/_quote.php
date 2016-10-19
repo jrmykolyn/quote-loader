9d1d19d2ec1d2f52454080b6426643a54c5c49ed
Jesse Mykolyn <jrmykolyn@gmail.com>
2016-10-19 17:14:26 -0400

    Updated project to display 'quote year' if present in CSV data.

--- includes/quote/_quote.php
+++ includes/quote/_quote.php
@@ -3,6 +3,19 @@
 		<?= $quote_arr[1]; ?>
 	</span>
 	<footer class="elem--hide js--trans-in">
-		<cite><?= $quote_arr[0]; ?></cite>
+		<cite>
+			<?php
+				// Print out quote author.
+				echo $quote_arr[0];
+
+				// Print out quote year if avail.
+				if ( isset( $quote_arr[2] ) && !empty( $quote_arr[2] ) ) {
+					echo ", ";
+					echo "<em>";
+					echo $quote_arr[2];
+					echo "</em>";
+				}
+			?>
+		</cite>
 	</footer>
 </blockquote>
