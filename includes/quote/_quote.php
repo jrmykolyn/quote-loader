2dc62fcdddd04270341ac73b6366a171323b2c28
Jesse Mykolyn <jrmykolyn@gmail.com>
2016-10-18 23:02:00 -0400

    Initial commit for 'quote reader' microsite/project.

---
+++ includes/quote/_quote.php
@@ -0,0 +1,6 @@
+<blockquote>
+	<?= $quote_arr[1]; ?>
+	<footer>
+		<cite><?= $quote_arr[0]; ?></cite>
+	</footer>
+</blockquote>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <!-- VENDOR STYLES -->  
+    <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre:500,600,700|Raleway:500,500i,700,700i" rel="stylesheet">
+    <!-- PROJECT STYLES -->
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- VENDOR SCRIPTS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- PROJECT SCRIPTS -->
+    <script type="text/javascript" src="js/main.js"></script>
+  </head>
+  <body>
+    <header class="header-wrap">
+      <nav class="nav-wrap"></nav>
+    </header>
+    <main class="main-content">
+      <section class="quote-wrap">
+      <?php
+        $data = file_get_contents( './data/quotes.csv' );
+        $data_arr = array_filter( explode( "\n", $data ), strlen );
+
+        $quote_index = rand( 0, ( count($data_arr) - 1 ) );
+        $quote = $data_arr[$quote_index];
+        $quote_arr = explode( '|||' , $quote );
+
+        include_once( './includes/quote/_quote.php' );
+      ?>
+      </section><!-- .quote-wrap -->
+    </main><!-- .main-content-->
+    <footer class="footer-wrap"></footer>
+  </body>
+</html>
